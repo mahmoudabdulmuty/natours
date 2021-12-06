c2e5945cce604b96acc0204b7abe95b5b8662c36
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-12-06 23:33:57 +0200

    adding heading to header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
+import logo from '../img/logo-white.png';
+
 export default function App() {
 	return (
 		<>
-			<header className="header"></header>
+			<header className="header">
+				<div className="header__img-box">
+					<img className="header__img" src={logo} alt="logo" />
+				</div>
+				<div className="header__text-box">
+					<h1 className="header__heading">
+						<span className="heading__main">outdoors</span>
+						<span className="heading__sub">is where life happens</span>
+					</h1>
+				</div>
+			</header>
 		</>
 	);
 }
