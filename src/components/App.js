c7b9735875afe0c96ea919684c6bf5043f4e603e
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-12-13 06:04:06 +0200

    adding BEM Classes

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,11 @@ export default function App() {
 					<img className="header__img" src={logo} alt="logo" />
 				</div>
 				<div className="header__text-box">
-					<h1 className="header__heading">
-						<span className="heading__main">outdoors</span>
-						<span className="heading__sub">is where life happens</span>
+					<h1 className="heading-primary">
+						<span className="heading-primary--main">outdoors</span>
+						<span className="heading-primary--sub">is where life happens</span>
 					</h1>
-					<a href="/" className="btn btn-white btn-animated">
+					<a href="/" className="btn btn--white btn--animated">
 						discover our tours
 					</a>
 				</div>
